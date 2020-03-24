9aaf08027bf99e9f7ac167d6d4a7ba312d3353e3
Elismar13 <elismarsilva.5@outlook.com>
2020-03-24 20:28:59 -0300

    :pencil: database body done

---
+++ backend/src/database/migrations/20200324201647_create_incidents.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable( 'incidents', function (table) {
+      table.increments();   //Chave primária
+
+      table.string('title').notNullable(); 
+      table.string('description').notNullable();  
+      table.decimal('value').notNullable();  
+
+    //Coluna para saber quem criou a causa (relacionamento SQL)
+      table.string('ong_id').notNullable();  
+    
+      //Chave estrangeira para o campo 'ong_id'
+      table.foreing('ong_id').reference('id').inTable('ongs');
+    } )
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('incidents');
+  };
+  
