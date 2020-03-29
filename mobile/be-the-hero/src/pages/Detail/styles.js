b6fa0252333c2d86d4081550042e535b1d9ae98f
Elismar Sivla <elismarsilva.5@outlook.com>
2020-03-29 12:12:35 -0300

    :fire: Mobile complete

--- mobile/be-the-hero/src/pages/Detail/styles.js
+++ mobile/be-the-hero/src/pages/Detail/styles.js
@@ -1,73 +1,78 @@
-import { StyleSheet } from 'react-native'
-import Constants from 'expo-constants';
+import {
+	StyleSheet
+}
 
-export default StyleSheet.create({
-  constainer: {
-    flex: 1,
+from "react-native";
+import Constants from "expo-constants";
+export default StyleSheet.create( {
+	container: {
+    flex: 1, 
     paddingHorizontal: 24,
-    paddingTop: Constants.statusBarHeight + 20,
+    paddingTop: Constants.statusBarHeight + 20
   },
 
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-
-  headerText: {
-    fontSize: 15,
-    color: '#737380',
-  },
-
-  headerTextBold: {
-    fontWeight: 'bold',
-  },
-
-  title: {
-    fontSize: 30,
-    marginBottom: 16,
-    marginTop: 48,
-    color: '#13131a',
-    fontWeight: 'bold',
-  },
-
-  description: {
-    fontSize: 16,
-    lineHeight: 24,
-    color: '#737380'
-  },
-
-  incidentList: {
-    marginTop: 32,
+    flexDirection: "row", 
+    justifyContent: "space-between", 
+    alignItems: "center"
   },
 
   incident: {
-    padding:24,
+    padding: 24,
     borderRadius: 8,
     backgroundColor: "#FFF",
     marginBottom: 16,
+    marginTop: 48
   },
-
-  incidentProperty: {
-    fontSize: 18,
-    color: '#41414d',
-    fontWeight: 'bold',
+  
+	incidentProperty: {
+		fontSize: 14, color: "#41414d", fontWeight: "bold", marginTop: 24
   },
 
   incidentValue: {
-    marginTop: 8,
-    fontSize: 15,
-    color: '#737380'
+		marginTop: 8, fontSize: 15, color: "#737380"
+  },
+  
+  contactBox: {
+    padding: 24,
+    borderRadius: 8, 
+    backgroundColor: "#FFF", 
+    marginBottom: 16
+  }, 
+
+  heroTitle: {
+    fontWeight: "bold", 
+    fontSize: 20, 
+    color: "#13131a", 
+    lineHeight: 30
   },
 
-  detailsButton: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  heroDescription: {
+    fontSize: 15, 
+    color: "#737380",
+    marginTop: 16
   },
 
+  actions: {
+    marginTop: 16, 
+    flexDirection: "row", 
+    justifyContent: "space-between"
+  },
 
-  detailsButtonText: {
-    
-  }
-});
+  action: {
+    backgroundColor: "#e02041", 
+    borderRadius: 8, 
+    height: 50, 
+    width: "48%", 
+    justifyContent: "center", 
+    alignItems: "center"
+  },
+
+  actionText: {
+    color: "#FFF", 
+    fontSize: 15, 
+    fontWeight: "bold"
+	}
+}
+
+);
